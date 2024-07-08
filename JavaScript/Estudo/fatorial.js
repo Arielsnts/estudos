4fdff0d56abeeeaff4c2fa49add8dd5fce3a8395
Ariel Santos <165219923+Arielsnts@users.noreply.github.com>
2024-07-08 11:21:16 -0300

    alteração no código

--- JavaScript/Estudo/fatorial.js
+++ JavaScript/Estudo/fatorial.js
@@ -1,22 +1,33 @@
 const readline = require('readline');
-
-function fatorial(num) {
-    let resultado = 1;
-    for (let i = 1; i <= num; i++) {
-        resultado *= i;
-    }
-    return resultado;
-}
+const { randomInt } = require('crypto');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('Escolha um número inteiro: ', (input) => {
-    const escolha = parseInt(input);
-    const resposta = fatorial(escolha);
+let cont = 0;
 
-    console.log(`O fatorial de ${escolha} é ${resposta}!`);
-    rl.close();
-});
+function jogo() {
+    rl.question('ÍMPAR ou PAR? [I/P]: ', (esc) => {
+        esc = esc.toUpperCase().trim();
+        rl.question('Digite um número inteiro de 1 a 10: ', (numInput) => {
+            const num = parseInt(numInput);
+            const maq = randomInt(1, 11); // Gera um número aleatório entre 1 e 10
+            const soma = num + maq;
+
+            if ((soma % 2 === 0 && esc === 'P') || (soma % 2 === 1 && esc === 'I')) {
+                console.log(`Você escolheu ${num} e eu escolhi ${maq}, que resulta em ${soma}. VOCÊ GANHOU!`);
+                cont++;
+                jogo(); // Joga novamente
+            } else {
+                console.log(`Você escolheu ${num} e eu escolhi ${maq}, que resulta em ${soma}. VOCÊ PERDEU!`);
+                console.log(`O número de vitórias seguidas foi ${cont}!`);
+                rl.close();
+            }
+        });
+    });
+}
+
+// Iniciar o jogo
+jogo();
