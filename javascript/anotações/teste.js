72080713c420dbdf23c660a6962e5862aeaa8f8d
Ariel Santos <165219923+Arielsnts@users.noreply.github.com>
2024-07-17 14:41:08 -0300

    .

--- javascript/anotações/teste.js
+++ javascript/anotações/teste.js
@@ -1,33 +1,6 @@
-const readline = require('readline');
-const { randomInt } = require('crypto');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let cont = 0;
-
-function jogo() {
-    rl.question('ÍMPAR ou PAR? [I/P]: ', (esc) => {
-        esc = esc.toUpperCase().trim();
-        rl.question('Digite um número inteiro de 1 a 10: ', (numInput) => {
-            const num = parseInt(numInput);
-            const maq = randomInt(1, 11); // Gera um número aleatório entre 1 e 10
-            const soma = num + maq;
-
-            if ((soma % 2 === 0 && esc === 'P') || (soma % 2 === 1 && esc === 'I')) {
-                console.log(`Você escolheu ${num} e eu escolhi ${maq}, que resulta em ${soma}. VOCÊ GANHOU!`);
-                cont++;
-                jogo(); // Joga novamente
-            } else {
-                console.log(`Você escolheu ${num} e eu escolhi ${maq}, que resulta em ${soma}. VOCÊ PERDEU!`);
-                console.log(`O número de vitórias seguidas foi ${cont}!`);
-                rl.close();
-            }
-        });
-    });
+function aleatorio(min, max) {
+    const fator = max - min + 1
+    return parseInt(Math.random() * fator) + min
 }
 
-// Iniciar o jogo
-jogo();
+console.log(aleatorio(5, 10))
