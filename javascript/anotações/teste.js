be8f51b56e87a55d6fbe602149f1bee8bcc5dc82
Ariel Santos <165219923+Arielsnts@users.noreply.github.com>
2024-07-26 08:52:44 -0300

    exercicio de faculdade

--- javascript/anotações/teste.js
+++ javascript/anotações/teste.js
@@ -1,5 +1,10 @@
-function f(x) {
-    return x ** 2
+function quadratica(a, b, c) {
+    const delta = b ** 2 - 4 * a * c
+    if (delta < 0) return "Não possui raízes reais!"
+
+    const raizX1 = (-b + Math.sqrt(delta)) / 2 * a
+    const raizX2 = (-b - Math.sqrt(delta)) / 2 * a
+    return `As raízes da equação são ${raizX1} e ${raizX2}!`
 }
 
-console.log(f(4))
+console.log(quadratica(1, -5, 6))
