817bdb2e81ef6b6523cc28cff365e197f14278b5
Ariel Santos <165219923+Arielsnts@users.noreply.github.com>
2024-08-02 11:22:22 -0300

    adição de exercício

--- javascript/anotações/media.js
+++ javascript/anotações/media.js
@@ -1,22 +1,21 @@
-function calcularMedia(n1, n2, n3) {
-    return (n1 + n2 + n3) / 3;
+const media = (f, n1, n2, n3, p1 = 1, p2 = 1, p3 = 1) => f(n1, n2, n3, p1, p2, p3)
+
+const situacao = (carga, faltas, media) => {
+  if (media < 5) {
+    return "reprovado"
+  } else if (carga == 4 && faltas > 15) {
+    return "reprovado"
+  } else if (carga == 4 && faltas >= 8 && media < 7) {
+    return "reprovado"
+  } else if (carga == 6 && faltas > 22) {
+    return "reprovado"
+  } else if (carga == 6 && faltas >= 11 && media < 7) {
+    return "reprovado"
+  } else if (carga == 2 && faltas > 7) {
+    return "reprovado"
+  } else if (carga == 2 && faltas >= 3 && media < 7) {
+    return "reprovado"
+  } else return "aprovado"
 }
 
-function determinarSituacao(media, faltas) {
-    if (media >= 7 && faltas <= 15) {
-        return 'Aprovado!';
-    } else if (media >= 5 && faltas <= 7) {
-        return 'Aprovado!';
-    } else {
-        return 'Reprovado!';
-    }
-}
-
-const n1 = 6;
-const n2 = 8;
-const n3 = 5;
-const faltas = 5;
-const media = calcularMedia(n1, n2, n3);
-const situacao = determinarSituacao(media, faltas);
-
-console.log(`Sua média foi ${media.toFixed(2)}, seu número de faltas foi ${faltas} e você foi ${situacao}`);
+console.log(situacao(4, 2, media((n1, n2, n3, p1, p2, p3) => (n1*p1 + n2*p2 + n3*p3) / (p1+ p2+ p3) , 4, 5, 6, 1, 1, 1)))
