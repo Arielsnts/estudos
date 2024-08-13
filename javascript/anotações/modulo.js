10953f61d1c736ea28eef233b61f296484b50683
Ariel Santos <165219923+Arielsnts@users.noreply.github.com>
2024-08-13 10:32:33 -0300

    update

--- javascript/anotações/modulo.js
+++ javascript/anotações/modulo.js
@@ -1,2 +1,5 @@
 const somaF = (f) => (x) => (y) => (z) => f(x) + f(y) + f(z)
 console.log(somaF((x) => x < 0 ? -x : x)(-3)(2)(1))
+
+const min = (a, b, c) => a(c) < b(c) ? a : b
+console.log(min((x) => x ** 3, (y) => y * 3, 4)(-1))
