a0fa44d5dfef013d7837123f3d531e8109406924
Ariel Santos <165219923+Arielsnts@users.noreply.github.com>
2024-08-20 15:45:39 -0300

    correção de exercicio

--- javascript/repetições/script.js
+++ javascript/repetições/script.js
@@ -1,22 +1,25 @@
 function contar() {
-    var inicio = Number(document.getElementById('inicio').value)
-    var fim = Number(document.getElementById('fim').value)
-    var passo = Number(document.getElementById('passo').value)
-    var resultado = document.getElementById('resultado')
+    var inicio = Number(document.getElementById('inicio').value);
+    var fim = Number(document.getElementById('fim').value);
+    var passo = Number(document.getElementById('passo').value);
+    var resultado = document.getElementById('resultado');
 
-    resultado.innerHTML = `Contagem: `
-    if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
-        window.alert('ERRO! Dados insuficientes!')
-        resultado.innerHTML = `Contagem: `
+    resultado.innerHTML = `Contagem: `;
+
+    if (isNaN(inicio) || isNaN(fim) || isNaN(passo) || passo <= 0) {
+        window.alert('ERRO! Dados insuficientes ou passo inválido!');
+        resultado.innerHTML = `Contagem: `;
     } else {
         if (inicio < fim) {
+            // Contagem crescente
             for (let contagem = inicio; contagem <= fim; contagem += passo) {
-                resultado.innerHTML += `${contagem} ` 
+                resultado.innerHTML += `${contagem} `;
             }
         } else {
+            // Contagem decrescente
             for (let contagem = inicio; contagem >= fim; contagem -= passo) {
-                resultado.innerHTML += `${contagem} ` 
+                resultado.innerHTML += `${contagem} `;
             }
         }
-    } 
+    }
 }
