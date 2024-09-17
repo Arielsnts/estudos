c0bf75f8cd4591077cfd9a5c1e067d4d9647621e
Ariel Santos <165219923+Arielsnts@users.noreply.github.com>
2024-09-17 10:41:29 -0300

    update

--- javascript/anotações/simulaProva.js
+++ javascript/anotações/simulaProva.js
@@ -1,51 +1,42 @@
-function processarStringPacientes(string) {
-    const listaA = string.match(/O imc de [A-Za-z]+ é \d+(\.\d+)?/g)
-    const listaB = listaA.map(str => {
+const inputStr = "O imc de Fred é 25.3. O imc de Maria é 20.2. O imc de Ana é 15.5. O imc de Paulo é 32.4"
+
+function processarPacientes(string) {
+    const aLista = string.match(/O imc de [A-Za-z]+ é \d+(\.\d+)?/g)
+    const bLista = aLista.map(str => {
         const [, nome, imc] = str.match(/O imc de (\w+) é (\d+(\.\d+)?)/)
         return {nome, imc: parseFloat(imc)}
     })
-    return listaB
+    return bLista
 }
 
-const inputStr = "O imc de Fred é 25.3. O imc de Maria é 20.2. O imc de Ana é 15.5. O imc de Paulo é 32.4"
-const pacientesProcessados = Object.freeze(processarStringPacientes(inputStr))
-
-const ordenarPacientePorIMC = (registro) => {
-    return [...registro].sort((a, b) => a.imc - b.imc)
+function ordenarPacientePorIMC(lista) {
+    return [...lista].sort((a, b) => a.imc - b.imc)
 }
 
-const maiorEmenorPacientes = (registro) => {
-    const ordem = ordenarPacientePorIMC(registro)
+function maiorEmenorPacientes(lista) {
+    const copia = [...lista]
+    const ordem = ordenarPacientePorIMC(copia)
     return {menor: ordem[0].nome, maior: ordem[ordem.length - 1].nome}
 }
 
-const classificarPacientes = (registro) => {
-    return registro.map(paciente => ({
+function classificarPacientes(lista) {
+    return lista.map(paciente => ({
         ...paciente,
         risco: paciente.imc < 18.5
-        ? "abaixo do peso"
+        ? "abaixo"
         : paciente.imc < 24.9
-        ? "peso normal"
+        ? "normal"
         : paciente.imc < 29.9
-        ? "sobrepeso"
-        :"obesidade"
+        ? "acima"
+        : "obesidade"
     }))
 }
 
-const calcularMediaIMC = (registro) => {
-    const lista = [...registro]
-    return parseFloat((lista.reduce((acc, x) => acc + x.imc, 0) / lista.length).toFixed(1))
+function calcularMediaIMC(lista) {
+    const copia = [...lista]
+    return parseFloat((copia.reduce((acc, x) => acc + x.imc, 0) / copia.length).toFixed(1))
 }
 
-// questão 1
-console.log(processarStringPacientes(inputStr))
-// questão 2
-console.log(ordenarPacientePorIMC(pacientesProcessados))
-// questão 3
-console.log(maiorEmenorPacientes(pacientesProcessados))
-// questão 4
-console.log(classificarPacientes(pacientesProcessados))
-// questão 5
-console.log(calcularMediaIMC(pacientesProcessados))
-// teste final
+const pacientesProcessados = Object.freeze(processarPacientes(inputStr))
+
 console.log(pacientesProcessados)
